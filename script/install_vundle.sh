97b2e45b6b81f3cfd5f753d7249dd07670aa60a6
jenchieh <jcs090218@gmail.com>
2017-12-31 16:44:34 +0800

    add install vundle script record.

---
+++ script/install_vundle.sh
@@ -0,0 +1,14 @@
+# ========================================================================
+# $File: install_vundle.sh $
+# $Date: 2017-12-31 16:40:26 $
+# $Revision: $
+# $Creator: Jen-Chieh Shen $
+# $Notice: See LICENSE.txt for modification and distribution information
+#                   Copyright © 2017 by Shen, Jen-Chieh $
+# ========================================================================
+
+
+# SOURCE(jenchieh): https://github.com/VundleVim/Vundle.vim
+
+# Clone it from repo
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
